a39936d2556694d0d535d5275af51d7a6e7a8b37
Surya Ambrose <surya.ambrose@gmail.com>
2016-07-23 15:53:50 +0200

    AI subpackage unit test

---
+++ test/mockup.py
@@ -0,0 +1,69 @@
+from boardgame.gamemodel import GameModel, GameState
+
+
+class MockUpGameModel(GameModel):
+	"""Fake GameModel for test purposes"""
+
+	@staticmethod
+	def getPossibleMoves(state):
+		if state.depth >= 2:
+			return range(3)
+		elif state.depth == 1:
+			return range(2)
+		else:
+			return []
+
+	@staticmethod
+	def estimateNextState(state, move):
+		if state.depth >= 2:
+			if move == 0:
+				s = MockUpNonFinalGameState()
+				s.depth = 1
+				return s
+			elif move == 1:
+				return MockUpTieGameState()
+			elif move == 2:
+				s = MockUpNonFinalGameState()
+				s.depth = state.depth - 1
+				return s
+		elif state.depth == 1:
+			if move == 0:
+				return MockUpFinalGameState()
+			elif move == 1:
+				return MockUpTieGameState()
+
+class MockUpFinalGameState(GameState):
+	"""Fake final (no tie) GameState for test purposes"""
+
+	# Special property for the mockup
+	depth = 0
+
+	def isFinal(self):
+		return True
+
+	def isTie(self):
+		return False
+
+class MockUpTieGameState(GameState):
+	"""Fake tie GameState for test purposes"""
+
+	# Special property for the mockup
+	depth = 0
+
+	def isFinal(self):
+		return True
+
+	def isTie(self):
+		return True
+
+class MockUpNonFinalGameState(GameState):
+	"""Fake non final GameState for test purposes"""
+
+	# Special property for the mockup
+	depth = 1
+
+	def isFinal(self):
+		return False
+
+	def isTie(self):
+		return False
---
+++ test/test_ai.py
@@ -0,0 +1,40 @@
+import unittest
+
+from boardgame.ai import *
+from boardgame.ai.player import AiPlayer
+
+from mockup import MockUpNonFinalGameState, MockUpGameModel
+
+class MiniMaxTest(unittest.TestCase):
+	def setUp(self):
+		self.algo = minimax.MiniMax(MockUpGameModel)
+
+	def testRun1(self):
+		state = MockUpNonFinalGameState()
+		res = self.algo.getBestNextMove(state)
+		assert(res == 0)
+
+	def testRun2(self):
+		state = MockUpNonFinalGameState()
+		state.depth = 2
+		res = self.algo.getBestNextMove(state)
+		assert(res == 1)
+
+	def testRun3(self):
+		state = MockUpNonFinalGameState()
+		state.depth = 3
+		res = self.algo.getBestNextMove(state)
+		assert(res in [1,2])
+
+class AiPlayerTest(unittest.TestCase):
+	def setUp(self):
+		algo = minimax.MiniMax(MockUpGameModel)
+		self.player = AiPlayer(algo)
+
+	def testCreate(self):
+		assert(True) # Just make sure player creation is ok
+
+	def testPlay(self):
+		state = MockUpNonFinalGameState()
+		res = self.player.play(state)
+		assert(res == 0)
