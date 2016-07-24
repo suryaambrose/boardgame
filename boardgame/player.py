157100c0ed482b01284db543740ca9986ba76c38
Surya Ambrose <surya.ambrose@gmail.com>
2016-07-24 17:09:15 +0200

    Human player abstract class

--- boardgame/player.py
+++ boardgame/player.py
@@ -16,3 +16,17 @@ class Player(object):
 		@state : Current state of the game (GameState)
 		"""
 		raise NotImplementedError()
+
+class HumanPlayer(Player):
+	"""
+	Base class for human players
+
+	@name         : player's name (str)
+	@game_viewer  : Game displayer class (GameViewer)
+	"""
+	def __init__(self, name, game_viewer):
+		super(HumanPlayer, self).__init__(name)
+		self.game_viewer = game_viewer
+
+	def play(self, state):
+		self.game_viewer.showState(state)
