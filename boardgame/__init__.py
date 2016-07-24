f3ce6f35cf19c60cc0f92a4ba9bdc0250a427a64
Surya Ambrose <surya.ambrose@gmail.com>
2016-07-24 17:01:58 +0200

    Package description

---
+++ boardgame/__init__.py
@@ -0,0 +1,12 @@
+"""
+Boardgame is a Python package gathering several simple boardgames like
+TicTacToe or Connect 4. It is meant to be played by humans or by programs.
+
+Boardgame's modules contains base class which can be inherited to add new games,
+whereas subpackages contains actual game implementation or ai algorithms.
+"""
+
+DESCRIPTION = "Boardgame is a Python package gathering several simple \
+boardgames like TicTacToe or Connect 4"
+
+VERSION = 0.1
