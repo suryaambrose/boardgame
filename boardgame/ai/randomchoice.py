470aaf6ab440e483bf504bdbf20e2a5e253be334
Surya Ambrose <surya.ambrose@gmail.com>
2016-07-24 19:47:08 +0200

    Add RandomChoice AI

---
+++ boardgame/ai/randomchoice.py
@@ -0,0 +1,20 @@
+import random
+
+class RandomChoice(object):
+	"""
+	Implements a Random game algorithm
+
+	This algorithm is simply playing random moves. It is particularly handy for
+	game testing.
+	"""
+	def __init__(self, game):
+		"""
+		Create Random
+
+		@game   :  Game model
+		"""
+		self._game = game
+
+	def getBestNextMove(self, state):
+		options = self._game.getPossibleMoves(state)
+		return random.choice(options)
