ba70fd83667317a9e3e80fe69460ffbd2e2c4a43
Surya Ambrose <surya.ambrose@gmail.com>
2017-04-30 01:17:32 +0200

    Change default number of players

--- boardgame/commands/connectfour_commands.py
+++ boardgame/commands/connectfour_commands.py
@@ -3,7 +3,7 @@ from boardgame.connectfour import makeConnectFour
 DESCRIPTION = "Starts a ConnectFour game"
 
 def make_command_parser(parser):
-	parser.add_argument("--ai-players", default=0, type=int)
-	parser.add_argument("--human-players", default=0, type=int)
+	parser.add_argument("--ai-players", default=1, type=int)
+	parser.add_argument("--human-players", default=1, type=int)
 	parser.set_defaults(game_maker=makeConnectFour)
 	return parser
